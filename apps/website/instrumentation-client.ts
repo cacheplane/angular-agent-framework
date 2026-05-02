3ee158f6a5a2eeb05f99b23987db955d874f7dd4
Brian Love <brian@liveloveapp.com>
2026-05-02 10:43:20 -0700

    fix(website): allow anonymous PostHog analytics

--- apps/website/instrumentation-client.ts
+++ apps/website/instrumentation-client.ts
@@ -13,5 +13,6 @@ if (shouldCaptureAnalytics({ token, captureLocal, host: browserHost })) {
     api_host: normalizePostHogHost(process.env.NEXT_PUBLIC_POSTHOG_HOST),
     defaults: '2026-01-30',
     capture_pageview: true,
+    person_profiles: 'always',
   });
 }
