b344e9a5607ae3c1ad1875b85911bef03296fec2
Brian Love <brian@liveloveapp.com>
2026-05-13 09:44:59 -0700

    feat(ui-react): add ThemeToggle component

--- libs/ui-react/src/index.ts
+++ libs/ui-react/src/index.ts
@@ -4,3 +4,4 @@ export type { Theme } from '@ngaf/design-tokens';
 export { ThemeProvider, useTheme, type ThemeProviderProps } from './lib/theme-context';
 export { ThemedFrame, type ThemedFrameProps } from './lib/themed-frame';
 export { useEmbeddedTheme } from './lib/use-embedded-theme';
+export { ThemeToggle } from './lib/theme-toggle';
