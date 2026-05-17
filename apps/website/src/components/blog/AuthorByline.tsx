09c79899f77ce4b9a03de2e709fb3960299382be
Brian Love <brian@liveloveapp.com>
2026-05-17 08:33:37 -0700

    feat(website): add AuthorByline component

---
+++ apps/website/src/components/blog/AuthorByline.tsx
@@ -0,0 +1,30 @@
+import type { Author } from '../../lib/blog-authors';
+
+export function AuthorByline({ author }: { author: Author }) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 12,
+        fontSize: 14,
+        opacity: 0.8,
+      }}
+    >
+      {author.avatar ? (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={author.avatar}
+          alt={`${author.name} avatar`}
+          width={32}
+          height={32}
+          style={{ borderRadius: '50%' }}
+        />
+      ) : null}
+      <div>
+        <span style={{ fontWeight: 600 }}>{author.name}</span>
+        {author.role ? <span style={{ opacity: 0.7 }}> · {author.role}</span> : null}
+      </div>
+    </div>
+  );
+}
